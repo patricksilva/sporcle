763ede3f721052026f24f8f4e80c2d950116151b
Patrick Alex <patrickalex@gmail.com>
2016-07-23 23:46:32 -0300

    Add class Tela

---
+++ Tela.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.*;
+/**
+	This class was a challenge requested on ACADEMIA DO JAVA, ajtf96.
+	The main objective was to use arrays and Java's control flow statements.
+	@author:	Patrick Alex Freitas da Silva
+	@date: 2016-06-23
+*/
+class Tela {
+	
+	public static void main(String[] args){
+		
+		Sporcle jogoSporcle = new Sporcle();
+		
+	}
+	
+	public String getPlayerName(){
+		return prompt("Player name: ");
+	}
+}
